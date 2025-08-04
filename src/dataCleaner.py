dfed4c688ced947c73794fa9f609f90154ad6c10
Eclipse <Eclips77@users.noreply.github.com>
2025-08-04 10:22:04 +0300

    Fix typo in docstring for convert_to_lowercase method

--- src/dataCleaner.py
+++ src/dataCleaner.py
@@ -29,7 +29,7 @@ class DataCleaner:
         """"Set the dataframe content to a lowercase letters.
 
         Returns:
-             pd.DataFarame: with lowercase letters .
+             pd.DataFrame: with lowercase letters .
         """
 
         self.df = self.df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
