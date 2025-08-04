5ea9357c687e689514b9258febf1ef9fb5720e43
Eclipse <Eclips77@users.noreply.github.com>
2025-08-04 15:34:23 +0300

    Add DataLoader import to AppManager and enhance DataCleaner with punctuation removal and NaN handling methods

---
+++ managers/appManager.py
@@ -0,0 +1,3 @@
+from src.dataAnalyzer import DataAnalyzer
+from src.dataCleaner import DataCleaner
+from src.dataLoader import DataLoader
--- src/dataCleaner.py
+++ src/dataCleaner.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import string
 
 class DataCleaner:
     """Class for cleaning and preprocessing data.
@@ -15,16 +16,28 @@ class DataCleaner:
         """
         self.df = df
 
-    def clean_data(self) -> pd.DataFrame:
-        """Remove rows with missing values and duplicate rows.
+    def return_required_columns(self,required_columns : list) -> pd.DataFrame:
+        """Remove columns that is not important to.
 
         Returns:
-            pd.DataFrame: Cleaned DataFrame with no missing values or duplicates.
+            pd.DataFrame: Cleaned DataFrame with only the required columns.
         """
-        
-        self.df.drop_duplicates(inplace=True)
+        self.df = self.df[required_columns]
         return self.df
     
+    def remove_punctuation(self, column_name: str) -> pd.DataFrame:
+        """Remove punctuation from a specific column in the DataFrame.
+        
+        Args:
+            column_name (str): The name of the column to remove punctuation from.
+            
+        Returns:
+            pd.DataFrame: DataFrame with punctuation removed from the specified column.
+        """
+        translator = str.maketrans('', '', string.punctuation)
+        self.df[column_name] = self.df[column_name].apply(lambda x: x.translate(translator) if isinstance(x, str) else x)
+        return self.df
+
     def convert_to_lowercase(self,) -> pd.DataFrame:
         """"Set the dataframe content to a lowercase letters.
 
@@ -35,8 +48,20 @@ class DataCleaner:
         self.df = self.df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
         return self.df
     
+    def remove_none_classify_raws(self) -> pd.DataFrame:
+        """Remove rows that contain None or NaN values in classification columns.
+        
+        Returns:
+            pd.DataFrame: DataFrame with rows containing None/NaN values removed.
+        """
+        self.df = self.df.dropna()
+        return self.df
+        
 
     
 
-
+df = pd.read_csv(r"C:/Users/brdwn/Desktop/my_projects/Python/AppsProjects/HateFilter/Data/tweets_dataset.csv")
+dd = DataCleaner(df)
+x = dd.return_required_columns(["Text","Biased"])
+print(x.head(15))
 
