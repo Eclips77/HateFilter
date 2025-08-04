59866e38bcc85394deb848fb185e0b09d0d0f0b4
Eclipse <Eclips77@users.noreply.github.com>
2025-08-04 10:31:00 +0300

    Refactor DataAnalyzer class structure and improve docstring formatting

--- src/dataAnalyzer.py
+++ src/dataAnalyzer.py
@@ -1,6 +1,22 @@
-# import pandas as pd
+import pandas as pd
 
 
+class DataAnalyzer:
+    """Class for analyzing and processing data.
+    
+    This class provides methods to analyze  datasets ,
+    and return some statistics about the current data.
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        """Initialize the DataAnalyzer with a DataFrame.
+        
+        Args:
+            df (pd.DataFrame): The raw dataset to be analyzed.
+        """
+        self.df = df
+        
+      
 
 
 # c = df[category].value_counts().to_dict
